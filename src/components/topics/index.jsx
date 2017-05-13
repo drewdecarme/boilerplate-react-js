e1592fb18da15020d32d8e294e70f4484837ef5a
Drew DeCarme <drewdecarme@drews-macbook-air>
2017-05-13 18:36:56 -0400

    adds style to distinguish home and topics

---
+++ src/components/topics/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './index.scss';
+
+export default class Topics extends Component {
+  render() {
+    return (
+      <div styleName="topics">Welcome Topics</div>
+    );
+  }
+}
