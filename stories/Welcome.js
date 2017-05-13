ca6d2f666419ecee1dfc4b7439c7dd7ab0531f09
Drew DeCarme <drewdecarme@drews-macbook-air>
2017-05-13 14:57:07 -0400

    lint and initial fix of storybook

--- stories/Welcome.js
+++ stories/Welcome.js
@@ -2,42 +2,38 @@ import React from 'react';
 
 const styles = {
   main: {
-    margin: 15,
-    maxWidth: 600,
-    lineHeight: 1.4,
-    fontFamily: '"Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif',
-  },
-
-  logo: {
-    width: 200,
-  },
-
-  link: {
-    color: '#1474f3',
-    textDecoration: 'none',
-    borderBottom: '1px solid #1474f3',
-    paddingBottom: 2,
-  },
-
-  code: {
-    fontSize: 15,
-    fontWeight: 600,
-    padding: "2px 5px",
-    border: "1px solid #eae9e9",
-    borderRadius: 4,
-    backgroundColor: '#f3f2f2',
-    color: '#3a3a3a',
-  },
-
-  note: {
-    opacity: 0.5,
+    margin: 15
+    , maxWidth: 600
+    , lineHeight: 1.4
+    , fontFamily: '"Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif'
   }
+
+  , logo: { width: 200 }
+
+  , link: {
+    color: '#1474f3'
+    , textDecoration: 'none'
+    , borderBottom: '1px solid #1474f3'
+    , paddingBottom: 2
+  }
+
+  , code: {
+    fontSize: 15
+    , fontWeight: 600
+    , padding: '2px 5px'
+    , border: '1px solid #eae9e9'
+    , borderRadius: 4
+    , backgroundColor: '#f3f2f2'
+    , color: '#3a3a3a'
+  }
+
+  , note: { opacity: 0.5 }
 };
 
 export default class Welcome extends React.Component {
   showApp(e) {
     e.preventDefault();
-    if(this.props.showApp) this.props.showApp();
+    if (this.props.showApp) this.props.showApp();
   }
 
   render() {
@@ -49,9 +45,9 @@ export default class Welcome extends React.Component {
         </p>
         <p>
           We've added some basic stories inside the <code style={styles.code}>src/stories</code> directory.
-          <br/>
+          <br />
           A story is a single state of one or more UI components. You can have as many stories as you want.
-          <br/>
+          <br />
           (Basically a story is like a visual test case.)
         </p>
         <p>
@@ -67,12 +63,12 @@ export default class Welcome extends React.Component {
         </p>
         <p>
           This is just one thing you can do with Storybook.
-          <br/>
-          Have a look at the <a style={styles.link} href="https://github.com/kadirahq/react-storybook" target="_blank">React Storybook</a> repo for more information.
+          <br />
+          Have a look at the <a style={styles.link} href='https://github.com/kadirahq/react-storybook' target='_blank'>React Storybook</a> repo for more information.
         </p>
         <p style={styles.note}>
           <b>NOTE:</b>
-          <br/>
+          <br />
           Have a look at the <code style={styles.code}>.storybook/webpack.config.js</code> to add webpack
           loaders and plugins you are using in this project.
         </p>
